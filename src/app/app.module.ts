e14b73546fd378f57c096bb1e80dd62d0427713a
Julian Späth <julian.spaeth@wzw.tum.de>
2020-03-29 17:38:57 +0200

    Add password
    Username: admin
    Password: TUMvsCovid19!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ExplorerPageComponent} from './pages/explorer-page/explorer-page.component';
+import {AboutPageComponent} from './pages/about-page/about-page.component';
+import {HomePageComponent} from './pages/home-page/home-page.component';
 
-
-
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    ExplorerPageComponent,
+    AboutPageComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+
 })
 export class AppModule { }
