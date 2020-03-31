3c708ba05ff24aba362d3f3e6ef4c8632abba6ac
Julian Matschinske <julian.matschinske@wzw.tum.de>
2020-03-31 11:05:30 +0200

    Append missing slash

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
 
-const networkUrl = `${environment.backend}` + 'network';
+const networkUrl = `${environment.backend}` + 'network/';
 @Injectable({
   providedIn: 'root'
 })
