6d596e9290b6295fdb7437e08b840532caf2f286
Julian Matschinske <julian.matschinske@wzw.tum.de>
2020-04-11 00:15:34 +0200

    Add scores to super analysis
    Score was not displayed for the new super button

--- src/app/components/analysis-window/analysis-window.component.ts
+++ src/app/components/analysis-window/analysis-window.component.ts
@@ -135,7 +135,7 @@ export class AnalysisWindowComponent implements OnInit, OnChanges {
           }));
         await Promise.all(promises);
 
-        this.tableHasScores = ['trustrank', 'closeness', 'degree', 'quick'].indexOf(this.task.info.algorithm) !== -1;
+        this.tableHasScores = ['trustrank', 'closeness', 'degree', 'quick', 'super'].indexOf(this.task.info.algorithm) !== -1;
         if (this.tableHasScores) {
           this.toggleNormalization(true);
         }
