06ee214a4546663ca3ef79dfd1edbcf64b1f52e9
Julian Matschinske <julian@vyze.ai>
2020-11-05 16:20:21 +0100

    Make sidebar optional

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,11 @@
 export interface IConfig {
   legendUrl: string;
   legendClass: string;
+  showLeftSidebar: boolean;
 }
 
 export const defaultConfig: IConfig = {
   legendUrl: 'https://exbio.wzw.tum.de/covex/assets/leg1.png',
   legendClass: 'legend',
+  showLeftSidebar: true,
 };
