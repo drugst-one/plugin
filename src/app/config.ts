3edc909321e1a867c27ee5b7f9ea8ca339992563
Zakaria Louadi <zakaria.louadi@tum.de>
2020-11-12 18:57:46 +0100

    config file2

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,18 +1,29 @@
 export interface IConfig {
   legendUrl: string;
   legendClass: string;
+  taskName: string;
   showOverview: boolean;
-  showFooter: boolean;
   showQuery: boolean;
   showFilter: boolean;
+  showItemSelector: boolean;
+  showSimpleAnalysis: boolean;
+  showAdvAnalysis: boolean;
+  showTasks: boolean;
+  showSelection: boolean;
+  showFooter: boolean;
 }
 
 export const defaultConfig: IConfig = {
   legendUrl: 'https://exbio.wzw.tum.de/covex/assets/leg1.png',
+  taskName: 'Run Task X',
   legendClass: 'legend',
   showOverview: true,
   showQuery: true,
   showFilter: true,
-
-  showFooter: false,
+  showItemSelector: true,
+  showSimpleAnalysis:false,
+  showAdvAnalysis: true,
+  showSelection: true,
+  showTasks: false,
+  showFooter: true,
 };
