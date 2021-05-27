534cbc054977b19df5e9935c0bff2e4678d0a92d
AndiMajore <andi.majore@googlemail.com>
2021-05-27 17:10:01 +0200

    adaptive size continued

--- src/app/components/info-tile/info-tile.component.ts
+++ src/app/components/info-tile/info-tile.component.ts
@@ -6,18 +6,18 @@ import {AnalysisService} from '../../services/analysis/analysis.service';
 @Component({
   selector: 'app-info-tile',
   templateUrl: './info-tile.component.html',
-  styleUrls: ['./info-tile.component.scss']
+  styleUrls: ['./info-tile.component.scss', '../../pages/explorer-page/explorer-page.component.scss']
 })
 export class InfoTileComponent implements OnInit {
 
-  @Input()
-  public wrapper: Wrapper;
+  @Input() public wrapper: Wrapper;
+  @Input() smallStyle: boolean;
 
   constructor(public analysis: AnalysisService) { }
 
   ngOnInit(): void {
   }
-  
+
   public beautify(url: string): string {
     if (url.startsWith('https://')) {
       url = url.substr('https://'.length);
