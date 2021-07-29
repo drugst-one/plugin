1ffcbcfb0bc88d24e811dad94291e9bd364c77d6
Michael Hartung <michi@Michaels-MacBook-Pro.local>
2021-07-29 11:01:17 +0200

    query node in analysis network and more query term

--- src/app/components/query-tile/query-tile.component.ts
+++ src/app/components/query-tile/query-tile.component.ts
@@ -14,14 +14,22 @@ export class QueryTileComponent {
 
   querySearch(term: string, item: Wrapper) {
     term = term.toLowerCase();
-    const data = item.data as Node;
-    if (data.netexId === undefined) {
-      return data.label.toLowerCase().indexOf(term) > -1 || data.id.toLowerCase().indexOf(term) > -1 
-    } else {
-      data.ensg = data.ensg.map(x => x.toLowerCase())
-      return data.symbol.toLowerCase().indexOf(term) > -1 || data.uniprotAc.toLowerCase().indexOf(term) > -1 || 
-        data.label.toLowerCase().indexOf(term) > -1 || data.ensg.includes(term) || data.id.toLowerCase().indexOf(term) > -1 ;
-    }
+    const data = JSON.parse(JSON.stringify(item.data));
+    // add possible missing attributes to not throw errors
+    if (data.ensg === undefined) {data.ensg = []};
+    if (data.groupName === undefined) {data.groupName = ''};
+    if (data.type === undefined) {data.type = ''};
+    if (data.symbol === undefined) {data.symbol = ''};
+    if (data.proteinName === undefined) {data.proteinName = ''};
+    if (data.uniprotAc === undefined) {data.uniprotAc = ''};
+    if (data.drugId === undefined) {data.drugId = ''};
+
+    data.ensg = data.ensg.map(x => x.toLowerCase())
+    return data.symbol.toLowerCase().indexOf(term) > -1 || data.uniprotAc.toLowerCase().indexOf(term) > -1 || 
+      data.label.toLowerCase().indexOf(term) > -1 || data.ensg.includes(term) || data.id.toLowerCase().indexOf(term) > -1 
+      || data.proteinName.toLowerCase().indexOf(term) > -1 || data.type.toLowerCase().indexOf(term) > -1 || 
+      data.groupName.toLowerCase().indexOf(term) > -1 || data.drugId.toLowerCase().indexOf(term) > -1;
+  
   }
 
   select(item) {
