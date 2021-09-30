caba0a3f3dafed4ccc90b3b5a7d2012fdae087e5
AndiMajore <andi.majore@googlemail.com>
2021-09-30 18:06:38 +0200

    left background panel color as option for white part of pi-chart

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -174,8 +174,9 @@ export function pieChartContextRenderer({ctx, x, y, state: {selected, hover}, st
     }
     // draw white background circle
     ctx.beginPath();
-    // alternatively window.getComputedStyle(document.documentElement).getPropertyValue('--drgstn-panel');?
     ctx.fillStyle = "white"
+    // or fill like background of graph panel
+    // ctx.fillStyle= window.getComputedStyle(document.documentElement).getPropertyValue('--drgstn-panel');
     ctx.arc(x, y, style.size, 0, 2 * Math.PI,false);
     ctx.fill();
     ctx.stroke();
