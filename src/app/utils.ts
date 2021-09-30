7b04578b40271608da18c231357975c9699e9a60
AndiMajore <andi.majore@googlemail.com>
2021-09-30 17:34:36 +0200

    fixed render order and shadow application for pi-charts

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,7 +1,7 @@
 // From https://stackoverflow.com/a/27709336/3850564
 
-import { ɵisListLikeIterable } from "@angular/core";
-import { Node } from "./interfaces";
+import {ɵisListLikeIterable} from "@angular/core";
+import {Node} from "./interfaces";
 
 export function getGradientColor(startColor: string, endColor: string, percent: number) {
   // strip the leading # if it's there
@@ -59,16 +59,16 @@ export function removeUnderscoreFromKeys(obj) {
 }
 
 // https://gist.github.com/whitlockjc/9363016
-function trim (str) {
-  return str.replace(/^\s+|\s+$/gm,'');
+function trim(str) {
+  return str.replace(/^\s+|\s+$/gm, '');
 }
 
-export function rgbaToHex (rgba) {
+export function rgbaToHex(rgba) {
   const inParts = rgba.substring(rgba.indexOf("(")).split(","),
-      r = parseInt(trim(inParts[0].substring(1)), 10),
-      g = parseInt(trim(inParts[1]), 10),
-      b = parseInt(trim(inParts[2]), 10),
-      a: number = parseFloat(parseFloat(trim(inParts[3].substring(0, inParts[3].length - 1))).toFixed(2));
+    r = parseInt(trim(inParts[0].substring(1)), 10),
+    g = parseInt(trim(inParts[1]), 10),
+    b = parseInt(trim(inParts[2]), 10),
+    a: number = parseFloat(parseFloat(trim(inParts[3].substring(0, inParts[3].length - 1))).toFixed(2));
   const outParts = [
     r.toString(16),
     g.toString(16),
@@ -94,17 +94,17 @@ function componentToHex(c) {
 
 export function rgbToHex(rgb) {
   const inParts = rgb.substring(rgb.indexOf("(")).split(","),
-      r = parseInt(trim(inParts[0].substring(1)), 10),
-      g = parseInt(trim(inParts[1]), 10),
-      b = parseInt(trim(inParts[2]), 10);
+    r = parseInt(trim(inParts[0].substring(1)), 10),
+    g = parseInt(trim(inParts[1]), 10),
+    b = parseInt(trim(inParts[2]), 10);
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 // https://stackoverflow.com/questions/1573053/javascript-function-to-convert-color-names-to-hex-codes/47355187#47355187
-export function standardizeColor(str){
-	var ctx = document.createElement("canvas").getContext("2d");
-	ctx.fillStyle = str;
-	return ctx.fillStyle.toString();
+export function standardizeColor(str) {
+  var ctx = document.createElement("canvas").getContext("2d");
+  ctx.fillStyle = str;
+  return ctx.fillStyle.toString();
 }
 
 export function removeDuplicateObjectsFromList(nodes: Node[], attribute: string): Node[] {
@@ -126,7 +126,7 @@ export function removeDuplicateObjectsFromList(nodes: Node[], attribute: string)
  * @param type - type of the document.
  */
 export function downLoadFile(data: any, type: string, fmt: string) {
-  let blob = new Blob([data], { type: type});
+  let blob = new Blob([data], {type: type});
   var a = document.createElement("a");
   a.href = URL.createObjectURL(blob);
   a.download = `drugstone_network_${new Date().getTime()}.${fmt}`;
@@ -135,7 +135,12 @@ export function downLoadFile(data: any, type: string, fmt: string) {
 
 export function RGBAtoRGB(rgbaString) {
   const rgbaStringSplit = rgbaString.slice(5, -1).split(",");
-  const RGBA = {red: rgbaStringSplit[0], green: rgbaStringSplit[1], blue: rgbaStringSplit[2], alpha: rgbaStringSplit[3]};
+  const RGBA = {
+    red: rgbaStringSplit[0],
+    green: rgbaStringSplit[1],
+    blue: rgbaStringSplit[2],
+    alpha: rgbaStringSplit[3]
+  };
   // assume white background
   const bg = {red: 255, green: 255, blue: 255};
   const RGB = {red: undefined, green: undefined, blue: undefined};
@@ -146,9 +151,9 @@ export function RGBAtoRGB(rgbaString) {
   return `rgb(${RGB.red},${RGB.green},${RGB.blue})`;
 }
 
-export function pieChartContextRenderer({ ctx, x, y, state: { selected, hover }, style, label }) {
+export function pieChartContextRenderer({ctx, x, y, state: {selected, hover}, style, label}) {
   console.log(style)
-  ctx.drawPieLabel = function(style, x, y, label) {
+  ctx.drawPieLabel = function (style, x, y, label) {
     ctx.font = "normal 12px sans-serif";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
@@ -156,35 +161,47 @@ export function pieChartContextRenderer({ ctx, x, y, state: { selected, hover },
     ctx.fillText(label, x, y + style.size + 12);
   }
 
-  ctx.drawPie = function(style, x, y) {
+  ctx.drawPie = function (style, x, y) {
     const total = 1;
 
     // draw shadow
     if (style.shadow) {
+      ctx.save()
       ctx.shadowColor = style.shadowColor;
       ctx.shadowOffsetX = style.shadowX;
       ctx.shadowOffsetY = style.shadowY;
       ctx.shadowBlur = 10;
     }
+    // draw white background circle
+    ctx.beginPath();
+    ctx.fillStyle = window.getComputedStyle(document.documentElement).getPropertyValue('--drgstn-background');
+    ctx.arc(x, y, style.size, 0, 2 * Math.PI,false);
+    ctx.fill();
+    ctx.stroke();
 
+    // prepare pi-chart
     ctx.fillStyle = style.color ? style.color : 'rgba(255, 0, 0, 1)';
     // set alpha value to 1
     // ctx.fillStyle = ctx.fillStyle.replace(/[^,]+(?=\))/, '1')
     ctx.fillStyle = RGBAtoRGB(ctx.fillStyle)
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
     ctx.beginPath();
     ctx.moveTo(x, y);
-    const len = style.opacity/total * 2 * Math.PI;
-    ctx.arc(x , y, style.size, 0, 0 + len, false);
+    const len = style.opacity / total * 2 * Math.PI;
+    ctx.arc(x, y, style.size, 0, 0 + len, false);
     ctx.lineTo(x, y);
     ctx.fill();
+    if (style.shadow) {
+      // removing shadow application of future fill or stroke calls
+      ctx.restore();
+    }
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = selected ? 3 : 2;
     if (style.opacity !== total) {
       // avoid the inner line when circle is complete
       ctx.stroke();
     }
 
-    // draw a cricle
+    // draw the surrounding border circle
     ctx.beginPath();
     ctx.arc(x, y, style.size, 0, 2 * Math.PI);
     ctx.stroke();
