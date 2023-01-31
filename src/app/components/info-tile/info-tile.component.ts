289263c6aba27278c190881736c0c8e87eb43067
AndiMajore <andi.majore@googlemail.com>
2023-01-31 17:46:57 +0100

    added modular linkout line to info panel

--- src/app/components/info-tile/info-tile.component.ts
+++ src/app/components/info-tile/info-tile.component.ts
@@ -3,6 +3,7 @@ import {DrugstoneConfigService} from 'src/app/services/drugstone-config/drugston
 import {Wrapper} from '../../interfaces';
 import {AnalysisService} from '../../services/analysis/analysis.service';
 import {NetworkHandlerService} from '../../services/network-handler/network-handler.service';
+import {validateComponent} from 'codelyzer/walkerFactory/walkerFn';
 
 
 @Component({
@@ -14,6 +15,8 @@ export class InfoTileComponent implements OnInit {
   @Input() public wrapper: Wrapper;
   @Input() public expressions: any;
 
+  public linkoutMap = {iid: 'IID'};
+
   constructor(public drugstoneConfig: DrugstoneConfigService, public analysis: AnalysisService, public networkHandler: NetworkHandlerService) {
 
   }
@@ -22,7 +25,7 @@ export class InfoTileComponent implements OnInit {
   }
 
   public getExpressionScore() {
-    return this.expressions[this.wrapper.id]
+    return this.expressions[this.wrapper.id];
   }
 
   public beautify(url: string): string {
@@ -45,5 +48,37 @@ export class InfoTileComponent implements OnInit {
       n--;
     }
     return out;
+  }
+
+  showLinks() {
+    const idSpace = this.drugstoneConfig.currentConfig().identifier;
+    const iidActive = (['symbol', 'uniprot', 'entrez'].includes(idSpace) && this.wrapper.data[idSpace] != null) || this.wrapper.data.uniprot != null;
+    return iidActive;
+  }
+
+  showLinkout(target) {
+    const idSpace = this.drugstoneConfig.currentConfig().identifier;
+    switch (target) {
+      case 'iid':
+        return (['symbol', 'uniprot', 'entrez'].includes(idSpace) && this.wrapper.data[idSpace] != null) || this.wrapper.data.uniprot != null;
+    }
+    return false;
+  }
+
+  getIIDQuery() {
+    const idSpace = this.drugstoneConfig.currentConfig().identifier;
+    if (['symbol', 'uniprot', 'entrez'].includes(idSpace) && this.wrapper.data[idSpace] != null) {
+      return this.wrapper.data[idSpace][0];
+    }
+    return this.wrapper.data.uniprot[0];
+  }
+
+  getLinkoutURL(target) {
+    // const idSpace = this.drugstoneConfig.currentConfig().identifier;
+    switch (target) {
+      case 'iid':
+        return 'http://iid.ophid.utoronto.ca/SearchPPIs/protein/' + this.getIIDQuery();
+    }
+    return '';
   }
 }
