b9ab2cc9bb723e6698e475afc91026e5f2cd1b75
Hartung, Michael <michael.hartung@uni-hamburg.de>
2023-04-27 19:09:14 +0200

    link-out for drugs

--- src/app/components/info-tile/info-tile.component.ts
+++ src/app/components/info-tile/info-tile.component.ts
@@ -1,40 +1,49 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {DrugstoneConfigService} from 'src/app/services/drugstone-config/drugstone-config.service';
-import {Wrapper} from '../../interfaces';
-import {AnalysisService} from '../../services/analysis/analysis.service';
-import {NetworkHandlerService} from '../../services/network-handler/network-handler.service';
-import {validateComponent} from 'codelyzer/walkerFactory/walkerFn';
-
+import { Component, Input, OnInit } from "@angular/core";
+import { DrugstoneConfigService } from "src/app/services/drugstone-config/drugstone-config.service";
+import { Wrapper } from "../../interfaces";
+import { AnalysisService } from "../../services/analysis/analysis.service";
+import { NetworkHandlerService } from "../../services/network-handler/network-handler.service";
+import { validateComponent } from "codelyzer/walkerFactory/walkerFn";
 
 @Component({
-  selector: 'app-info-tile',
-  templateUrl: './info-tile.component.html',
-  styleUrls: ['./info-tile.component.scss', '../../pages/explorer-page/explorer-page.component.scss']
+  selector: "app-info-tile",
+  templateUrl: "./info-tile.component.html",
+  styleUrls: [
+    "./info-tile.component.scss",
+    "../../pages/explorer-page/explorer-page.component.scss",
+  ],
 })
 export class InfoTileComponent implements OnInit {
-  @Input() public wrapper: Wrapper;
+  @Input() set wrapper(wrapperObject: Wrapper) {
+    this._wrapper = wrapperObject;
+    this.checkIfShowLinks();
+  }
   @Input() public expressions: any;
 
-  public linkoutMap = {iid: 'IID'};
+  public linkoutMap = { iid: "IID" };
 
-  constructor(public drugstoneConfig: DrugstoneConfigService, public analysis: AnalysisService, public networkHandler: NetworkHandlerService) {
+  public _showLinks: boolean = false;
+  public _wrapper?: Wrapper ;
 
-  }
+  constructor(
+    public drugstoneConfig: DrugstoneConfigService,
+    public analysis: AnalysisService,
+    public networkHandler: NetworkHandlerService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public getExpressionScore() {
-    return this.expressions[this.wrapper.id];
+    return this.expressions[this._wrapper.id];
   }
 
   public beautify(url: string): string {
-    if (url.startsWith('https://')) {
-      url = url.substr('https://'.length);
-    } else if (url.startsWith('http://')) {
-      url = url.substr('http://'.length);
+    if (url.startsWith("https://")) {
+      url = url.substr("https://".length);
+    } else if (url.startsWith("http://")) {
+      url = url.substr("http://".length);
     }
-    const slashPos = url.indexOf('/');
+    const slashPos = url.indexOf("/");
     if (slashPos !== -1) {
       return url.substr(0, slashPos);
     }
@@ -42,43 +51,57 @@ export class InfoTileComponent implements OnInit {
   }
 
   writeZeros(n: number) {
-    let out = '';
+    let out = "";
     while (n > 0) {
-      out += '0';
+      out += "0";
       n--;
     }
     return out;
   }
 
-  showLinks() {
+  public checkIfShowLinks() {
     const idSpace = this.drugstoneConfig.currentConfig().identifier;
-    const iidActive = (['symbol', 'uniprot', 'entrez'].includes(idSpace) && this.wrapper.data[idSpace] != null) || this.wrapper.data.uniprot != null;
-    return iidActive;
+    this._showLinks =
+      (["symbol", "uniprot", "entrez"].includes(idSpace) &&
+      this._wrapper.data[idSpace] != null) ||
+    this._wrapper.data.uniprot != null ||
+    this._wrapper.data.type === "drug";
+    
   }
 
   showLinkout(target) {
     const idSpace = this.drugstoneConfig.currentConfig().identifier;
     switch (target) {
-      case 'iid':
-        return (['symbol', 'uniprot', 'entrez'].includes(idSpace) && this.wrapper.data[idSpace] != null) || this.wrapper.data.uniprot != null;
+      case "iid":
+        return (
+          (["symbol", "uniprot", "entrez"].includes(idSpace) &&
+            this._wrapper.data[idSpace] != null) ||
+          this._wrapper.data.uniprot != null
+        );
     }
     return false;
   }
 
   getIIDQuery() {
     const idSpace = this.drugstoneConfig.currentConfig().identifier;
-    if (['symbol', 'uniprot', 'entrez'].includes(idSpace) && this.wrapper.data[idSpace] != null) {
-      return this.wrapper.data[idSpace][0];
+    if (
+      ["symbol", "uniprot", "entrez"].includes(idSpace) &&
+      this._wrapper.data[idSpace] != null
+    ) {
+      return this._wrapper.data[idSpace][0];
     }
-    return this.wrapper.data.uniprot[0];
+    return this._wrapper.data.uniprot[0];
   }
 
   getLinkoutURL(target) {
     // const idSpace = this.drugstoneConfig.currentConfig().identifier;
     switch (target) {
-      case 'iid':
-        return 'http://iid.ophid.utoronto.ca/SearchPPIs/protein/' + this.getIIDQuery();
+      case "iid":
+        return (
+          "http://iid.ophid.utoronto.ca/SearchPPIs/protein/" +
+          this.getIIDQuery()
+        );
     }
-    return '';
+    return "";
   }
 }
