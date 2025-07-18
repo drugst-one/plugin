7ba34b26e7a22214d0ed7e490691b85a758cc410
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-07-18 11:59:05 +0200

    setting correct path to dev api again

--- src/environments/environment.dev_server.ts
+++ src/environments/environment.dev_server.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backend: 'https://api.drugst.one/'
+  backend: 'https://dev.api.drugst.one/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = 'v2.1.1-rc11';
+export const version = 'v2.1.1-rc12';
